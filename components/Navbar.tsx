bd6a8886be71e02e13847ff3b877793ce8b2dce7
RomualdP <romuald.piquet@gmail.com>
2023-10-09 14:36:26 +0200

    fixed the tailwind compile issue

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,10 +8,7 @@ export default function Navbar({ user } : NavbarProps) {
   return (
     <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
       <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm text-foreground">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          {/* <Link
+          <Link
             href="/"
             className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
           >
@@ -36,7 +33,7 @@ export default function Navbar({ user } : NavbarProps) {
             >
               Login
             </Link>
-          )} */}
+          )}
       </div>
     </nav>
   )
