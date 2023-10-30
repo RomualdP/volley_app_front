a17e938246caf62963ae59dd9c65225b71b5d784
RomualdP <romuald.piquet@gmail.com>
2023-10-30 10:25:09 +0100

    added fake infos to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 
+import Card from '@/components/Card'
 import Link from 'next/link'
 
 export const dynamic = 'force-dynamic'
@@ -7,9 +8,15 @@ export const dynamic = 'force-dynamic'
 export default async function Index() {
  
   return (
-    <div className="w-full flex flex-col items-center">
-        <div className="flex justify-center text-center text-xs">
-        </div>
+    <div className="w-full max-w-4xl flex flex-col items-center p-4 text-sm text-foreground my-24 gap-4">
+        <Card>
+            <h2 className='font-anime-ace text-l font-bold mb-4'>Dernières infos</h2>
+            <p><b>30/10</b> Attention à votre consommation de bonbons avant les matchs</p>
+        </Card>
+        <Card>
+            <h2 className='font-anime-ace text-l font-bold mb-4'>Prochain match</h2>
+            <p>20/11 Lagny - Otis</p>
+        </Card>
       </div>
   )
 }
